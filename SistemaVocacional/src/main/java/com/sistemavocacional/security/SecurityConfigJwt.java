826ad7e10ce7fb187c91026765eb04c47c737e83
Christhian6998 <peralta6998@gmail.com>
2026-03-08 01:32:15 -0500

    Actualizacion de cors

--- SistemaVocacional/src/main/java/com/sistemavocacional/security/SecurityConfigJwt.java
+++ SistemaVocacional/src/main/java/com/sistemavocacional/security/SecurityConfigJwt.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,12 +28,13 @@ public class SecurityConfigJwt {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
     	http
-    	.cors(cors -> {})
+    	.cors(Customizer.withDefaults())
         .csrf(csrf -> csrf.disable())
         .sessionManagement(session ->
             session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         )
         .authorizeHttpRequests(auth -> auth
+        	.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
             .requestMatchers("/auth/login").permitAll()
             .requestMatchers("/usuario/registrarUsuario").permitAll()
             .requestMatchers("/usuario/listarUsuariosConsentimiento").hasRole("ADMIN")
